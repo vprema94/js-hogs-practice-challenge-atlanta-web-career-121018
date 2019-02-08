2eae0c841a21de16c94e47447576ba36ae61d831
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-07 21:13:01 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+//first thing that is run is the setupPage function, once the DOM has been loaded 
 document.addEventListener('DOMContentLoaded', setupPage)
+
+//variables for where the hogs will go & where the form is 
 const hogContainer = document.querySelector('#hog-container')
 const hogForm = document.querySelector('#form')
 
+//addFormHandler is run first, this gives the form a eventListener for submit, then all the piggies go on the page 
 function setupPage() {
     addFormHandler()
     renderAllHogs()
 } 
 
+//first the container for all the hogs is cleared out (this prevents from having duplicates if you want to use this function again later)
+//then 'getHogs()' brings us back JSON data from the url 
+//then for each hog, a 'renderHog' is performed
 function renderAllHogs() {
     hogContainer.textContent = ""
     getHogs().then(function(data){
@@ -14,10 +21,13 @@ function renderAllHogs() {
     })
 } 
 
+//fetches all the hogs, and turns it into JSON data
 function getHogs() {
     return fetch('http://localhost:3000/hogs').then(res => res.json())
 } 
 
+//takes in ONE hog and creates a "card" for that hog
+//then adds it to the hog container 
 function renderHog(hog) {
     let hogCard = document.createElement('div')
     hogCard.className = 'hog-card'
@@ -38,10 +48,12 @@ function renderHog(hog) {
 
             let hogGrease = document.createElement('input')
             hogGrease.type = 'checkbox'
+            //checks the box on the browser if the hog.greased value is true
             if (hog.greased) {
                 hogGrease.checked = true
             } 
             hogGrease.dataset.id = hog.id
+            //adds an event listener if this checkbox is checked
             hogGrease.addEventListener('change', changeGrease)
             greaseContainer.appendChild(hogGrease)
 
@@ -62,16 +74,19 @@ function renderHog(hog) {
             
             let deleteBtn = document.createElement('button')
             deleteBtn.textContent = 'Delete Me :('
+            //adds an event listener to the delete button, NOTE: the arrow function is vital bc it allows you to pass in the hog object to removeHog
             deleteBtn.addEventListener("click", () => removeHog(hog))
             deleteContainer.appendChild(deleteBtn)
 
     hogContainer.appendChild(hogCard)
 } 
 
+//adds an event listener to the new hog form 
 function addFormHandler() {
     hogForm.addEventListener('submit', newHog)
 }  
 
+//takes the input values from the hog form and sends it to createHog()
 function newHog() {
     event.preventDefault();
     
@@ -86,6 +101,7 @@ function newHog() {
     event.target.reset() // clears form
 } 
 
+//runs a fetch to POST a new hog in the database (using the user input from newHog())
 function createHog(name, specialty, medal, weight, img, grease) {
     return fetch('http://localhost:3000/hogs', {
         method: 'POST',
@@ -104,13 +120,17 @@ function createHog(name, specialty, medal, weight, img, grease) {
 } 
 
 function removeHog(hog) {
+    //the event.target is the button, the parent to that is the button container, the parent to that is the hog card (which is what we want to remove)
     let byeHog = event.target.parentElement.parentElement
+    //we remove here by telling THAT parent to delete its child
     byeHog.parentElement.removeChild(byeHog)
 
+    //we need the hog id to know which hog to delete from the database
     let id = hog.id 
     deleteHog(id) 
 } 
 
+//runs a fetch to DELETE the hog you selected using the id
 function deleteHog(id) {
     return fetch(`http://localhost:3000/hogs/${id}`,{
         method: 'DELETE',
@@ -123,12 +143,14 @@ function deleteHog(id) {
 } 
 
 function changeGrease() {
+    //newGrease is the new value of the check box (.checked function returns a true/false)
     let newGrease = event.target.checked
+    //the id is needed to update that specific hog in the database, i put it on the button for easy acces (see line 55)
     let id = event.target.dataset.id 
-    debugger
     updateGrease(id, newGrease) 
 }
 
+//runs a fetch to PATCH the hog who had the checkbox changed 
 function updateGrease(id, newGrease) {
     return fetch(`http://localhost:3000/hogs/${id}`,{
       method: 'PATCH',
